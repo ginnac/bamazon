e58807622365fa7192eff04714b1c10e8bd400fa
ginnac <ginnacampbell1990@gmail.com>
2019-08-02 20:39:00 -0600

    challenge 1 steps

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+//connecting to mysql
+
+
+//writing it to the console
+
+
+//let the "user" decide which item to buy and quantity - inquirer
+
+
+//if enough stock decrement stock
+
+
+
+//if not enough stock console log sorry not enough stock.
