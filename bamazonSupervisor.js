1f3e8208f5e137be2a31015865e007d3d6284571
ginnac <ginnacampbell1990@gmail.com>
2019-08-04 16:11:16 -0600

    verify connection

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -3,7 +3,30 @@ var mysql = require("mysql");
 var Table = require('cli-table');
 
 //create connection to mysql 
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon"
+  });
+  
 //and connect to mysql
+
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    // function to display the menue options
+
+    console.log("connected");
+  
+  });
 
 //inquirer to display the following choices to be selected by the user:
 
